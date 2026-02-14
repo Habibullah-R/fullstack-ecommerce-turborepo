38994acd2c0e00f1bf634def5364f2ae15909a2e
Habibullah-R <habibullahrashid150@gmail.com>
2026-02-14 21:15:51 +0500

    feat(product controller):added product controller logic in product service

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -1,4 +1,4 @@
-import type { Prisma } from "@repo/product-db";
+import { Prisma } from "@repo/product-db";
 import { prisma } from "@repo/product-db";
 import { Request, Response } from "express";
 
@@ -20,10 +20,74 @@ export const createProduct = async (req: Request, res: Response) => {
   const product = await prisma.product.create({ data });
   res.status(201).json(product);
 };
-export const updateProduct = async (req: Request, res: Response) => {};
-export const deleteProduct = async (req: Request, res: Response) => {};
-export const getProducts = async (req: Request, res: Response) => {
-  const products = await prisma.product.findMany()
-  return res.status(200).json(products)
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data: Prisma.ProductUpdateInput = req.body;
+  const updatedProduct = await prisma.product.update({
+    where: {
+      id: Number(id),
+    },
+    data,
+  });
+  return res.status(200).json(updatedProduct);
 };
-export const getProduct = async (req: Request, res: Response) => {};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data: Prisma.ProductUpdateInput = req.body;
+  const deletedProduct = await prisma.product.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  return res.status(200).json(deletedProduct);
+};
+
+export const getProducts = async (req: Request, res: Response) => {
+  const { search, sort, category, limit } = req.query;
+
+  const orderBy = (() => {
+    switch (sort) {
+      case "asc":
+        return { price: Prisma.SortOrder.asc };
+        break;
+
+      case "desc":
+        return { price: Prisma.SortOrder.desc };
+        break;
+
+      case "oldest":
+        return { createdAt: Prisma.SortOrder.asc };
+        break;
+      default:
+        return { createdAt: Prisma.SortOrder.desc };
+        break;
+    }
+  })();
+
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category as string,
+      },
+      name: {
+        contains: search as string,
+        mode: "insensitive",
+      },
+    },
+    orderBy,
+    take: limit ? Number(limit) : undefined,
+  });
+  return res.status(200).json(products);
+};
+
+export const getProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await prisma.product.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  return res.status(200).json(product);
+};
