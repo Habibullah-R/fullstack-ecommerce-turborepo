c4f652cd538c6585f0d97b7e1d407330a53c07ad
Habibullah-R <habibullahrashid150@gmail.com>
2026-02-15 17:36:43 +0500

    feat(stripe):configure stripe in order service

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { shouldBeUser } from "./middleware/auth.middleware.js";
+import stripe from "./utills/stripe.js";
 
 const app = new Hono();
 const PORT = 8002;
@@ -14,6 +15,25 @@ app.get("/", (c) => {
 app.get("/test",shouldBeUser, (c) => {
   return c.json({ message: "Payment service authenticated",userId:c.get("userId") }, 200);
 });
+
+app.post("/create-stripe-product",async(c)=>{
+  const res = await stripe.products.create({
+    id:"123",
+    name:"test product",
+    default_price_data:{
+      currency:"usd",
+      unit_amount:10*100
+    }
+  })
+  return c.json(res)
+})
+
+app.get("/stripe-product-price",async(c)=>{
+  const res = await stripe.prices.list({
+    product:"123"
+  })
+  return c.json(res)
+})
 
 serve(
   {
